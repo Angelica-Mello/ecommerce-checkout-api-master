0c24d34b48502d1dacd110707b485aaabc2001a4
Angélica Mello <angelicamoliveira@hotmail.com>
2021-04-18 18:30:17 -0300

    e-commerce checkout API

---
+++ src/main/java/com/angelicamello/ecommerce/checkout/config/StreamingConfig.java
@@ -0,0 +1,14 @@
+package com.angelicamello.ecommerce.checkout.config;
+
+import com.angelicamello.ecommerce.checkout.streaming.CheckoutCreatedSource;
+import com.angelicamello.ecommerce.checkout.streaming.PaymentPaidSink;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBinding(value = {
+        CheckoutCreatedSource.class,
+        PaymentPaidSink.class
+})
+public class StreamingConfig {
+}
---
+++ src/test/java/com/hatanaka/ecommerce/checkout/SpringIntegrationTest.java
@@ -0,0 +1,18 @@
+package com.hatanaka.ecommerce.checkout;
+
+import com.angelicamello.ecommerce.checkout.CheckoutApplication;
+import com.hatanaka.ecommerce.checkout.setup.PostgreSQLSetup;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@CucumberContextConfiguration
+@SpringBootTest(classes = {
+        CheckoutApplication.class,
+        PostgreSQLSetup.class
+}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 8888)
+public class SpringIntegrationTest {
+}
